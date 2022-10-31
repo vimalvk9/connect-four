9f77d4c5fe20067acf39722518d7f1ee5eb91007
Bhadkaashiq <116168527+Bhadkaashiq@users.noreply.github.com>
2022-10-31 23:00:51 +0530

    Update Driver.java

--- src/Driver.java
+++ src/Driver.java
@@ -1,11 +1,7 @@
 
 public class Driver {
-
-
     public static void main(String[] args) {
-
         Game game = new Game();
-        game.getBoard().printBoard();
         game.startGame();
 
     }
